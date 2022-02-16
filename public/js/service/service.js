0878d6de918ba44689ebf6a3c6b0dd043dff38f5
Maxime CAT <maximecat@outlook.fr>
2022-02-16 12:15:35 +0100

    service récupération json

--- public/js/service/service.js
+++ public/js/service/service.js
@@ -1,15 +1,34 @@
 import { Photographer } from "../models/Photographer.js"
+import { Video } from "../models/Video.js"
+import { Image } from "../models/Image"
 
 export function getPhotographers() {
     return fetch('public/datas/datas.json')
         .then((response) => response.json())
-        .then((datas) => datas.photographers.map((photographer) => new Photographer(photographer)))
+        .then((datas) => datas.photographers
+            .map((photographer) => {
+                return new Photographer(photographer)
+            }))
 }
 
 export function getPhotographer(photographerId) {
-
+    // Utiliser methode find
+    return getPhotographers()
+        .then((photographers) => photographers
+            .find((photographer) => photographer.id === Number(photographerId)))
 }
 
 export function getMedias(photographerId) {
-
+    // Utiliser méthode filter
+    return fetch('public/datas/datas.json')
+        .then((response) => response.json())
+        .then((datas) => datas.media
+            .filter((media) => media.photographerId === Number(photographerId))
+            .map((media) => {
+                if(media.image) {
+                    return new Image(media)
+                } else if(media.video) {
+                    return new Video(media)
+                }
+            }))
 }
