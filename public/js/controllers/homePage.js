47e7fe98b0dbfe72027e2a6fbe1230daddbcbe65
Maxime CAT <maximecat@outlook.fr>
2022-02-16 12:11:02 +0100

    Page d'acceuil import des photographes

--- public/js/controllers/homePage.js
+++ public/js/controllers/homePage.js
@@ -1,7 +1,7 @@
-import {getPhotographers} from "../service/service.js";
+import { getPhotographers } from "../service/service.js";
 
 getPhotographers()
-.then((photographers) => displayPhotographers(photographers))
+    .then((photographers) => displayPhotographers(photographers))
 
 // Parcour la liste des photographe et affiche l'HTML du photographe dans le body  (en dynamique)
 function displayPhotographers(photographers) {
@@ -11,11 +11,35 @@ function displayPhotographers(photographers) {
     // Boucles sur la liste des photographes
     for (const photographer of photographers) {
         // Pour chaque .. photographes, création d'une div
-        const div = document.createElement('div');
-        // Pour chaque .. photographe on met le nom du photographe dans la div crée
-        div.innerHTML = photographer.name;
+        const link = document.createElement('a');
+        const article = document.createElement('article');
+        const image = document.createElement('img');
+        const nom = document.createElement('h2');
+        nom.className = "nom";
+        const ville = document.createElement('div');
+        ville.className = "ville";
+        const description = document.createElement('div');
+        description.className = "description";
+        const prix = document.createElement('div');
+        prix.className = "prix";
+
+        link.appendChild(article);
+        article.appendChild(image);
+        article.appendChild(nom);
+        article.appendChild(ville);
+        article.appendChild(description);
+        article.appendChild(prix);
+
+        link.href = "/photographer-page.html?id=" + photographer.id;
+        image.src = "/public/images/Photographers/" + photographer.portrait;
+        nom.innerText = photographer.name;
+        ville.innerText = photographer.city + ", " + photographer.country;
+        description.innerText = photographer.tagline;
+        prix.innerText = photographer.price + "€/jour";
+        
+        
+
         // Pour chaque .. photographe on insere la div crée dans le conteneur (main)
-        photographersContainer.appendChild(div);
+        photographersContainer.appendChild(link);
     };
 }
-
