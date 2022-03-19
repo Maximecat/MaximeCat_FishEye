c6f5d0bf1452abf1a52e4dffe63d1ed6650c49a0
Maxime CAT <maximecat@outlook.fr>
2022-03-19 09:17:35 +0100

    Affichage des medias pages des photographes

--- public/js/controllers/photographerPage.js
+++ public/js/controllers/photographerPage.js
@@ -1,3 +1,5 @@
+import { MediaFactory } from "../service/MediaFactory.js";
+import { PhotographerFactory } from "../service/PhotographerFactory.js";
 import { getPhotographer, getMedias } from "../service/service.js";
 
 const urlSearchParams = new URLSearchParams(window.location.search);
@@ -6,52 +8,36 @@ const params = Object.fromEntries(urlSearchParams.entries());
 const photographerId = params.id;
 console.log(photographerId);
 
+// Fonction de récupération du photographe (par son numéro d'identifiant "Id")
 getPhotographer(photographerId)
-  .then(displayDescription)
+  .then(function (photographer){
+    // Récupération de la Factory -> PhotographerFactory
+    const photographerFactory = new PhotographerFactory(photographer);
 
+    // Création du header pour le photographe
+    //( à partir du model dans notre "class" -> 'PhotographerFactory' de la méthode createPhotographerHeader() )
+    photographerFactory.createPhotographerHeader();
+  })
+
+// Fonction de récupération des médias ( du photographe par son numéro d'identifiant "Id"), une fois fais on appel la fonction 'displayMedias' qui parcourir la liste des medias et les affiche dans notre div (en dynamique)
 getMedias(photographerId)
   .then(displayMedias)
 
-function displayDescription(photographer) {
-  const name = document.getElementById("name");
-  const city = document.getElementById("city");
-  const desc = document.getElementById("desc");
-  const img = document.getElementById("img");
-
-  name.innerText = photographer.name;
-  city.innerText = photographer.city + ", " + photographer.country;
-  desc.innerText = photographer.tagline;
-  img.src = "/public/images/Photographers/" + photographer.portrait;
-
-}
-
+// Fonction pour parcourir la liste des medias et les affiche dans notre div (en dynamique)
 function displayMedias(medias) {
 
+  // Récupération de la div ou seront affiché nos medias
   const photographMedias = document.getElementById('photograph-medias');
 
+  // Boucle sur la liste des medias
   for (const media of medias) {
 
-    const photographMedia = document.createElement('div');
-    photographMedia.className = "photograph-media";
-    const photographPicture = document.createElement('img');
-    photographPicture.className = "photograph-picture";
-    const aboutPicture = document.createElement('div');
-    aboutPicture.className = "about-picture";
-    const pictureTitle = document.createElement('div');
-    pictureTitle.className = "picture-title";
-    const pictureLikes = document.createElement('div');
-    pictureLikes.className = "picture-likes";
+    // Récupération de la Factory -> 'MediaFactory'
+    const mediaFactory = new MediaFactory(media);
 
-    photographPicture.src = "/public/images/" + media.photographerId + "/" + (media.image|| media.video)
-    pictureTitle.innerText = media.title;
-    pictureLikes.innerText = media.likes;
-
-
-    aboutPicture.appendChild(pictureTitle);
-    aboutPicture.appendChild(pictureLikes);
-    photographMedia.appendChild(photographPicture);
-    photographMedia.appendChild(aboutPicture);
-    photographMedias.appendChild(photographMedia);
+    // Dans notre div crée un nouvelle card ou un "enfant" a la précédente, t'en qu'il y a un nouveau media pour se photographe 
+    // ( à partir du model dans notre "class" -> 'MediaFactory' de la méthode createMediaCard() )
+    photographMedias.appendChild(mediaFactory.createMediaCard());
 
   }
 };
