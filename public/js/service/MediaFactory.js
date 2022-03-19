bf308aef9ae15da649e37f2fc04b75e20ebe34a0
Maxime CAT <maximecat@outlook.fr>
2022-03-19 09:20:11 +0100

    Model card d'un media

---
+++ public/js/service/MediaFactory.js
@@ -0,0 +1,42 @@
+export class MediaFactory {
+
+    constructor(media){
+        this.media = media
+    }
+
+    // Méthode pour crée une card affichant un media
+    createMediaCard() {
+        const photographMedia = document.createElement('div');
+        photographMedia.className = "photograph-media";
+        const photographPicture = document.createElement('img');
+        photographPicture.className = "photograph-picture";
+        const aboutPicture = document.createElement('div');
+        aboutPicture.className = "about-picture";
+        const pictureTitle = document.createElement('div');
+        pictureTitle.className = "picture-title";
+        const pictureLikes = document.createElement('div');
+        pictureLikes.className = "picture-likes";
+        const heartLikes = document.createElement('i');
+        heartLikes.className = "fa-solid fa-heart";
+
+
+        photographPicture.src = "/public/images/" + this.media.photographerId + "/" + (this.media.image|| this.media.video)
+        pictureTitle.innerText = this.media.title;
+        pictureLikes.innerText = this.media.likes + " ";
+
+
+        aboutPicture.appendChild(pictureTitle);
+        aboutPicture.appendChild(pictureLikes);
+        pictureLikes.appendChild(heartLikes);
+        photographMedia.appendChild(photographPicture);
+        photographMedia.appendChild(aboutPicture);
+
+        photographPicture.addEventListener('click', this.displayDialog);
+
+        return photographMedia;
+    }
+
+    displayDialog() {
+        console.log("la dialog es affichée")
+    }
+}
