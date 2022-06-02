73552ce11706f78906f426e7c4a2a376570feeef
Maxime CAT <maximecat@outlook.fr>
2022-06-02 15:55:01 +0200

    Model card d'un media et display modal du media

--- public/js/service/MediaFactory.js
+++ public/js/service/MediaFactory.js
@@ -11,30 +11,41 @@ export class MediaFactory {
     createMediaCard() {
         const photographMedia = document.createElement('div');
         photographMedia.className = "photograph-media";
+
         let photographPicture;
+
         if (this.media instanceof Image) {
             photographPicture = document.createElement('img');
         } else if (this.media instanceof Video) {
             photographPicture = document.createElement('video');
         }
+
         photographPicture.className = "photograph-picture";
-        photographPicture.alt = this.media.title;
+        photographPicture.alt = "Photo de : " + this.media.title;
+        photographPicture.tabIndex = 3;
+
         const aboutPicture = document.createElement('div');
         aboutPicture.className = "about-picture";
+
         const pictureTitle = document.createElement('div');
         pictureTitle.className = "picture-title";
+        pictureTitle.tabIndex = 3;
+
         const pictureLikes = document.createElement('div');
         pictureLikes.className = "picture-likes";
+
         const likes = document.createElement('span');
         likes.id = "likes-" + this.media.id;
         likes.className = 'like-margin';
+
         const heartLikes = document.createElement('i');
         heartLikes.className = "fa-solid fa-heart like-icon";
         heartLikes.id = "heart-likes-" + this.media.id;
-
+        heartLikes.tabIndex = 3;
 
         photographPicture.src = "/public/images/" + this.media.photographerId + "/" + (this.media.image|| this.media.video)
         photographPicture.id = "thumb-" + this.media.id;
+
         pictureTitle.innerText = this.media.title;
         likes.innerText = this.media.likes;
 
@@ -49,6 +60,11 @@ export class MediaFactory {
         photographPicture.addEventListener('click', () => {
             this.displayDialog();
         });
+        photographPicture.addEventListener('keypress', (e) => {
+            if(e.code === "Space" || e.code === "Enter") {
+                this.displayDialog();
+            }
+        });
 
         return photographMedia;
     }
@@ -61,7 +77,9 @@ export class MediaFactory {
 
         const mediaContainerLightBox = document.getElementById("media-container-lightbox");
         mediaContainerLightBox.innerHTML = null;
+
         let mediaToDisplay;
+
         if (this.media instanceof Image) {
             mediaToDisplay = this.createPhoto();
         } else if (this.media instanceof Video) {
@@ -76,20 +94,31 @@ export class MediaFactory {
 
     createPhoto() {
         const mediaToDisplay = document.createElement('img');
+
         mediaToDisplay.src = "/public/images/" + this.media.photographerId + "/" + this.media.image;
+        mediaToDisplay.alt = "Photo de :" + this.media.image;
         mediaToDisplay.id = this.media.id;
         mediaToDisplay.classList.add("picture-lightbox");
+        
         return mediaToDisplay;
     }
 
     createVideo() {
         const mediaToDisplay = document.createElement('video');
         mediaToDisplay.controls = true;
+
         const videoSource = document.createElement('source');
         videoSource.src = "/public/images/" + this.media.photographerId + "/" + this.media.video;
+
+        const alt = document.createElement('p');
+        alt.innerText = `Votre navigateur ne supporte pas la vidéo html. Voici un <a href="${videoSource.src}">lien vers la vidéo</a>`
+
         mediaToDisplay.appendChild(videoSource);
+        mediaToDisplay.appendChild(alt);
+
         mediaToDisplay.id = this.media.id;
         mediaToDisplay.classList.add("picture-lightbox");
+
         return mediaToDisplay;
     }
 }
