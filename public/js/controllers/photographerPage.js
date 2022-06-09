0e21329e76f6d0edf66a558c0ca5e075c185d55b
Maxime CAT <maximecat@outlook.fr>
2022-06-09 21:27:46 +0200

    Affichage dynamique des éléments page photographes

--- public/js/controllers/photographerPage.js
+++ public/js/controllers/photographerPage.js
@@ -4,79 +4,94 @@ import { getPhotographer, getMedias } from "../service/service.js";
 import { Video } from "../models/Video.js";
 import { Image } from "../models/Image.js";
 
+// Création d'un tableau pour les médias
 let medias = [];
+// Création d'un tableau pour les likes
 const mediaLikes = [];
 
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
+// Récupération de l'Id du photographe
 const photographerId = params.id;
-console.log(photographerId);
 
+// Application de la fonction de fermeture de modal des medias au click sur l'icon 'croix'
 const xmarkIcon = document.getElementById('close-button');
 xmarkIcon.addEventListener('click', closeModal);
 
+// Application de la fonction de parcours des médias (par la droite) au click sur l'icon 'flêche de droite'
 const right = document.getElementById('right');
 right.addEventListener('click', () => nextMedia('right'));
 
+// Application de la fonction de parcours des médias (par la gauche) au click sur l'icon 'flêche de gauche'
 const left = document.getElementById('left');
 left.addEventListener('click', () => nextMedia('left'));
 
-//------------------------------------------------
-
+// Application de la fonction d'ouverture du menu (nav) au click sur le bouton
 const iconOpenMenu = document.getElementById('menu-button');
 iconOpenMenu.addEventListener('click', openMenu);
 
+// Fonction d'ouverture du menu (nav)
 function openMenu() {
   const secondBloc = document.getElementById('second-part-menu');
   secondBloc.style.display = "flex";
 }
 
+// Fonction de fermeture du menu (nav)
 function closeMenu() {
   const secondBloc = document.getElementById('second-part-menu');
   secondBloc.style.display = "none";
 }
 
-//------------------------------------------------
-
+// Fonction de récupération d'un photographe par son id
 getPhotographer(photographerId)
   .then(function (photographer) {
 
     const photographerFactory = new PhotographerFactory(photographer);
     photographerFactory.createPhotographerHeader();
 
+    // Récupération du bouton de 'contact', au click apparition de la modal de contact
     const contactButton = document.getElementById('btn-contact');
     contactButton.addEventListener('click', () => displayModalForm(photographer));
 
+    // Récupération du prix du photographe (affichage dans la bannière de likes) 
     const photographerPrice = document.getElementById('price-banner');
     photographerPrice.innerText = photographer.price + "€ / jour";
   })
 
-
+// Fonction de récupération des médias du photographe par son id
 getMedias(photographerId)
   .then(displayMedias)
 
+// Fonction pour afficher les medias 
 function displayMedias(mediasFromFetch, criteria = "likes", sortLabel = "Popularité") {
 
   document.getElementById('sort-choice').innerText = sortLabel;
 
   medias = mediasFromFetch;
 
+  // Fonction de tri des medias avec en paramétre le 'critère' sélectionné
   sortMedias(criteria);
 
+  // Récupération de la balise d'affichage des medias
   const photographMedias = document.getElementById('photograph-medias');
   photographMedias.innerHTML = "";
 
+  // Boucle sur la liste des medias, création d'un nouvelle card pour chaque medias
   for (const media of medias) {
     const mediaFactory = new MediaFactory(media);
     photographMedias.appendChild(mediaFactory.createMediaCard());
   }
 
+  // Utilisation de la fonction pour faire apparaître le nombre total de likes
   displayTotalLikes();
 
+  // Récupération de l'icon like en dessous de chaque media
   const divsLikes = document.getElementsByClassName('like-icon');
+  // Boucle, au click pour chaque icon, utilisation de la fonction d'ajout ou de retrait de like
   for (const divLikes of divsLikes) {
     divLikes.addEventListener('click', switchLikes);
+    // Accessibilité ------- Ecoute sur les icons like, au clavier ajoute ou retire le like avec 'Entrer'
     divLikes.addEventListener('keypress', (e) => {
       if (e.code === "Enter") {
         switchLikes(e);
@@ -85,34 +100,38 @@ function displayMedias(mediasFromFetch, criteria = "likes", sortLabel = "Popular
   }
 }
 
+// Fonction d'apparition des medias trier
 function setSortEvent() {
+  // Récupération des boutons du menu déroulant
   const sortChoices = document.getElementsByClassName("sortBy");
+  // Boucle dans les choix
   for (const choice of sortChoices) {
+    // Ecoute du bouton, au click sur le choix, fermeture du menu
     choice.addEventListener('click', (e) => {
-
+      
       closeMenu();
-
+      
       if (e.target.innerText) {
         displayMedias(medias, e.target.id, e.target.innerText)
       }
     })
   }
 }
-
+// Utilisation de la fonction d'apparition des medias trier
 setSortEvent();
 
+// Fonction de tri des medias
 function sortMedias(criteria) {
   switch (criteria) {
+    // Si 'date' est sélectionné, trier par date
     case 'date':
-      console.log(criteria, medias);
       medias = medias.sort((a, b) => new Date(b[criteria]).getTime() - new Date(a[criteria]).getTime());
       break;
-
+    // Si 'title' est sélectionné, trier par titre dans l'ordre alphabétique
     case 'title':
-      console.log(criteria, medias);
       medias = medias.sort((a, b) => a[criteria].localeCompare(b[criteria]));
       break;
-
+    // Si 'likes' est sélectionné, trier par nombre de like (tri par défault)
     case 'likes':
     default:
       medias = medias.sort((a, b) => b[criteria] - a[criteria]);
@@ -120,13 +139,16 @@ function sortMedias(criteria) {
   }
 }
 
+// Fonction d'apparition du nombre total de likes
 function displayTotalLikes() {
   const monRes = medias.reduce((cumu, media) => cumu + media.likes, 0)
   const totalLikes = document.getElementById('total-likes');
   totalLikes.innerText = monRes;
 }
 
+// Fonction d'ajout ou de suppression de like
 function switchLikes(event) {
+
   const likeId = event.target.id;
   const mediaId = likeId.split('heart-likes-')[1];
   let indexToDelete = -1;
@@ -157,16 +179,18 @@ function switchLikes(event) {
   displayTotalLikes();
 }
 
-//--------------------------------------------------------------------------
-
+// Application de la fonction de fermeture du formulaire, au click sur l'icon de fermeture
 const closeFormButton = document.getElementById('close-img-button');
 closeFormButton.addEventListener('click', closeModalForm);
 
+// Application de la fonction de submit, au click sur le bouton 'Envoyer' du formulaire
 const contactForm = document.getElementById('contact_form');
 contactForm.addEventListener('submit', onSubmitContactForm);
 
+// Fonction de submit du formulaire
 function onSubmitContactForm(event) {
   event.preventDefault()
+  // Récupération des inputs et de l'expression régulière souhaité pour chaque inputs avec un message d'erreur éventuel
   const inputsConf = {
     prenom: {
       isValid: false,
@@ -194,11 +218,14 @@ function onSubmitContactForm(event) {
     }
   }
 
+  // Boucle des clé dans inputsConf
   for (const key in inputsConf) {
+    // Test de notre expression régulière des inputs 
     inputsConf[key].isValid = inputsConf[key].regExp.test(inputsConf[key].input.value);
     displayIsValidInput(inputsConf[key].isValid, inputsConf[key].input, inputsConf[key].error);
   }
 
+  // Si tout les inputs sont valide, affiche les valeurs dans la console
   if (
     inputsConf.prenom.isValid &&
     inputsConf.nom.isValid &&
@@ -212,6 +239,7 @@ function onSubmitContactForm(event) {
   }
 }
 
+// Fonction d'affichage de l'erreur ou de la validation pour l'input
 function displayIsValidInput(isValid, inputElement, errorMessage) {
   if (!isValid) {
     inputElement.parentElement.removeAttribute("valid");
@@ -224,6 +252,7 @@ function displayIsValidInput(isValid, inputElement, errorMessage) {
   }
 }
 
+// Fonction d'affichage de la modal du formulaire et du nom du photographe assosié
 function displayModalForm(photographer) {
   const contactModal = document.getElementById('contact_modal');
   contactModal.style.display = "block";
@@ -232,16 +261,19 @@ function displayModalForm(photographer) {
   nameProfil.innerText = photographer.name;
 }
 
+// Fonction de fermeture de la modal du formulaire
 function closeModalForm() {
   const contactModal = document.getElementById('contact_modal');
   contactModal.style.display = "none";
 }
 
+// Fonction de fermeture de la modal des medias agrandi
 function closeModal() {
   const modal = document.getElementById('lightbox-modal');
   modal.style.display = "none";
 }
 
+// Fonction pour parcourir les medias de gauche a droite avec les icons de flêche de notre modal
 function nextMedia(direction) {
   const pictureLightBox = document.getElementsByClassName("picture-lightbox");
   const titlePictureLightBox = document.getElementsByClassName("title-picture-lightbox");
@@ -269,6 +301,7 @@ function nextMedia(direction) {
 
   const mediaFactory = new MediaFactory(medias[nextIndex]);
 
+  // Si c'est une Image
   if (medias[index] instanceof Image) {
     if (medias[nextIndex] instanceof Image) {
       pictureLightBox[0].src = "public/images/" + medias[nextIndex].photographerId + "/" + medias[nextIndex].image;
@@ -277,6 +310,7 @@ function nextMedia(direction) {
       const videoElement = mediaFactory.createVideo();
       mediaContainer.appendChild(videoElement);
     }
+    // Si c'est une Vidéo
   } else if (medias[index] instanceof Video) {
     if (medias[nextIndex] instanceof Video) {
       pictureLightBox[0].firstChild().src = "public/images/" + medias[nextIndex].photographerId + "/" + medias[nextIndex].video;
