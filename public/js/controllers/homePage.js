5155c4555a605fbe0fd4f318f1a41d2db92f0fee
Maxime CAT <maximecat@outlook.fr>
2022-06-09 21:27:36 +0200

    Affichage des photographes page principal

--- public/js/controllers/homePage.js
+++ public/js/controllers/homePage.js
@@ -12,12 +12,9 @@ function displayPhotographers(photographers) {
 
     // Boucle sur la liste des photographes
     for (const photographer of photographers) {
-        // Récupération de la Factory -> 'PhotographerFactory'
+
         const photographerFactory = new PhotographerFactory(photographer);
-        // Récupération du model pour crée une card de photographe avec la méthode 'createPhotographerCard'
         const article = photographerFactory.createPhotographerCard();
-        // Dans notre main crée un nouvelle card ou un "enfant" a la précédente, t'en qu'il y a un nouveau photographe 
-        // ( à partir du model dans notre "class" -> 'PhotographerFactory' avec la méthode createPhotographerCard() )
         photographersContainer.appendChild(article);
         // Accessibilité ------- Pour chaque photographe la page est accessible à partir de sa card avec 'Espace' ou 'Entrer'
         setEvent(article, photographer);
@@ -26,9 +23,9 @@ function displayPhotographers(photographers) {
 
 // Fonction d'accessibilité à la page de photographe par sa card avec 'Espace ou 'Entrer'
 function setEvent(article, photographer) {
-    // Ecoute sur la card du photographe
+    
     article.addEventListener('keypress', (e) => {
-        // Si la touche utilisé est 'Espace' ou 'Entrer', redirection vers la page associé à se photographe par rapport à son id
+        // Accessibilité ------- Si la touche 'Espace' ou 'Entrer' est utilisé, redirection vers la page associé à se photographe par rapport à son id
         if(e.code === "Space") {
             document.location.href = "/photographer-page.html?id=" + photographer.id;
         }
